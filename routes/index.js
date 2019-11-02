b62ad08eed3ae3cfd55dd574e482f962dbce3101
hhiyeon <hhyyeon0214@gmail.com>
2019-11-02 21:59:59 +0900

    commit project sopkathon

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.use('/router', require('./router/index'));
+router.use('/router', require('./router/index'));
+router.use('/router', require('./router/index'));
+
+module.exports = router;

c37b8795abfa9dd583718c6aa1db53d79e085d5d
hhiyeon <hhyyeon0214@gmail.com>
2019-11-03 05:57:20 +0900

    commit party-index

--- routes/party/index.js
+++ routes/party/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 // /* GET home page. */
-// router.use('/order', require('./order/index'));
+router.use('/order', require('./order.js'));
 router.use('/detail', require('./detail.js'));
 router.use('/enroll', require('./enroll.js'));
 

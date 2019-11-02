2d09554476bcc12c9a58b43cb410f1df066ed1ea
hhiyeon <hhyyeon0214@gmail.com>
2019-11-03 02:20:20 +0900

    commit resMessage#1

--- module/utils/responseMessage.js
+++ module/utils/responseMessage.js
@@ -3,6 +3,8 @@ module.exports = {
     NULL_VALUE: "필요한 값이 없습니다",
     OUT_OF_VALUE: "파라미터 값이 잘못되었습니다",
 
+    SUCCESS_MONTH_PARTY: "이달의 행사 조회 성공",
+
     SIGNUP_FAIL: "회원가입 실패",
     SIGNUP_SUCCESS : "회원가입 성공",
     ALREADY_EXIST_USER: "이미 존재하는 아이디 입니다.",

45cf27a405aa35d05e758b37fce58b3b2c0e9135
Hyun-juhee <juhd1998@gmail.com>
2019-11-03 05:33:54 +0900

    commit enroll

---
+++ routes/party/enroll.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var router = express.Router();
+const moment = require('moment');
+
+const pool = require('../../module/pool');
+const utils = require('../../module/utils/utils');
+const statusCode = require('../../module/utils/statusCode');
+const responseMessage = require('../../module/utils/responseMessage');
+const table = 'party';
+
+partyEnroll = { //creatAt,,?
+    enroll : async({image, title, date, address, content, please, host, createdAt})=>{
+        const fields = 'image, title, date, address, content, please, host, createdAt';
+        const table = 'party';
+        const questions = '?,?,?,?,?,?,?,?';
+        const values = [image, title, date, address, content, please, host, createdAt];
+        const query = `INSERT INTO ${table}(${fields}) VALUES(${questions})`;
+        const result = await pool.queryParam_Parse(query,values);
+
+        if(!result){
+            return{
+                code: statusCode.BAD_REQUEST,
+                json: utils.successFalse(statusCode.BAD_REQUEST,responseMessage.ENROLL_FAIL)
+            };
+        }
+        return{
+            code : statusCode.OK,
+            json : utils.successTrue(statusCode.OK,responseMessage.ENROLL_SUCCESS)
+        };
+    }
+}
+
+router.post('/',async(req,res)=>{
+    const {image, title, date, address, content, please, host} = req.body;
+    const createdAt = moment().format('YYYY-MM-DD HH:mm:ss');
+    if(!image || !title || !date || !address || !content || !please || !host ||!createdAt){
+        res.status(statusCode.BAD_REQUEST)
+        .send(utils.successFalse(statusCode.BAD_REQUEST,responseMessage.NULL_VALUE))
+    }try {
+        const {code, json} = await partyEnroll.enroll({image, title, date, address, content, please,host, createdAt})
+        res.status(code).send(json);
+    }catch(err)
+    {
+        console.log(err);
+        res.status(statusCode.INTERNAL_SERVER_ERROR).send(utils.successFalse(responseMessage.ENROLL_FAIL));
+    }
+})
+module.exports = router;
